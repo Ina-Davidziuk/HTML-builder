a275147d5f6f891a6cb06fd369ab7f9bab4ee7b0
Ina-Davidziuk <ina.davidziuk@gmail.com>
2023-05-08 14:00:46 +0200

    fix:fix problems in task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,18 +2,17 @@ const fs = require('node:fs/promises');
 const { mkdir, copyFile, rm} = require('fs/promises');
 const path = require('node:path');
 
-
 const filesPath = path.join(__dirname, 'files');
 const filesCopyPath = path.join(__dirname, 'files-copy');
 
 async function copyDir () {
-        await rm (filesCopyPath, {force: true, recursive: true, maxRetries: 100})
-            await  mkdir(filesCopyPath, { recursive: true }, err => {
-                if(err){
-                console.error(err);
-                }
-                })
-                console.log('The folder is coppy sucsessfully')
+        await rm (filesCopyPath, {force: true, recursive: true, maxRetries: 100});
+        await  mkdir(filesCopyPath, { recursive: true }, err => {
+            if(err){
+            console.error(err);
+            }
+        });
+        console.log('The folder is coppy sucsessfully');
         const files = await fs.readdir(filesPath, {withFileTypes: true});
         for (const file of files) {
             const filesInOldFolder = path.join(__dirname, 'files', file.name);
